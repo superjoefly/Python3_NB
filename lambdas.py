1c564e82babc2b6371c3ab71707cd4da144a7ba1
superjoefly <superjoefly@gmail.com>
2018-05-29 17:07:40 -0600

    Completed lambda function example.

---
+++ lambdas.py
@@ -0,0 +1,14 @@
+"""Lambda Function Example"""
+
+# Define main:
+def main():
+    """The Main Function"""
+
+    # Define a lambda function:
+    answer = lambda x: x*7
+
+    # Call the lambda function:
+    print(answer(5))
+
+# Call main:
+main()
