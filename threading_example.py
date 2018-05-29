693b753882e3c6884b5cdaaadf04439ee005fefa
superjoefly <superjoefly@gmail.com>
2018-05-29 15:40:04 -0600

    Removed image; made adjustments to threading example.

--- threading_example.py
+++ threading_example.py
@@ -1,10 +1,18 @@
+"""Threading Example"""
+
+# The following program uses threading:
+
 # Import Modules
 import threading
 import time
 
+# The main() function creates a threading class and
+# starts the threads which run simultaneously:
 def main():
+    """This is the main function:"""
     # Create class:
     class MyMessenger(threading.Thread):
+        """This is the MyMessenger Class"""
         # Define run function:
         def run(self):
             # Loop to print thread name:
@@ -22,4 +30,5 @@ def main():
     x.start()
     y.start()
 
+# Call main:
 main()
