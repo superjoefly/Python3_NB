18fcb978d2cf7f2075b6ba73fbcb4363b280cf39
superjoefly <superjoefly@gmail.com>
2018-05-30 19:38:58 -0600

    Working on Images example.

--- image_manipulation/main.py
+++ image_manipulation/main.py
@@ -2,23 +2,65 @@
 from PIL import Image
 
 
+# Define main:
 def main():
-    # Open the image:
-    my_image = Image.open('flowers.jpg')
+    """The main function"""
+    # # Open the image:
+    # my_image = Image.open('flowers.jpg')
+    #
+    # # Display image size:
+    # print(my_image.size)
+    #
+    # # Display image format:
+    # print(my_image.format)
+    #
+    # # Display the image itself
+    # # (uses default image viewer):
+    # my_image.show()
+    #
+    # # Close the image:
+    # my_image.close()
 
-    # Display image size:
-    print(my_image.size)
+    ###############################
 
-    # Display image format:
-    print(my_image.format)
+    # # Image Croping:
+    #
+    # # Define the area (x, y TO x , y):
+    # area = (150, 200, 375, 600)
+    #
+    # # Open the image:
+    # my_image = Image.open('bella.jpg')
+    #
+    # # Crop the image:
+    # cropped_image = my_image.crop(area)
+    #
+    # # Show image:
+    # cropped_image.show()
+    #
+    # # Close image:
+    # my_image.close()
 
-    # Display the image itself
-    # (uses default image viewer):
+    ################################
+
+    # Image Overlay:
+
+    # Open the images:
+    my_image = Image.open('me.jpg')
+    ducky = Image.open('ducky.png')
+
+    # Coordinates must match exactly:
+    area = (350, 350, 500, 500)
+
+    # Paste image on top of other:
+    my_image.paste(ducky, area)
+
+    # Show the result image:
     my_image.show()
 
-
-
+    # Close both images:
     my_image.close()
+    ducky.close()
 
 
+# Call main:
 main()
