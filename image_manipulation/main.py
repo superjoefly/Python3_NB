74829afa16151ae60afa148a0e51a2096edd108c
superjoefly <superjoefly@gmail.com>
2018-05-31 21:27:40 -0600

    Completed channels examples.

--- image_manipulation/main.py
+++ image_manipulation/main.py
@@ -42,24 +42,88 @@ def main():
 
     ################################
 
-    # Image Overlay:
+    # # Image Overlay:
+    #
+    # # Open the images:
+    # my_image = Image.open('me.jpg')
+    # ducky = Image.open('ducky.png')
+    #
+    # # Coordinates must match exactly:
+    # area = (350, 350, 500, 500)
+    #
+    # # Paste image on top of other:
+    # my_image.paste(ducky, area)
+    #
+    # # Show the result image:
+    # my_image.show()
+    #
+    # # Close both images:
+    # my_image.close()
+    # ducky.close()
+
+    ################################
+
+    # Working with Channels:
 
     # Open the images:
-    my_image = Image.open('me.jpg')
-    ducky = Image.open('ducky.png')
+    view_image = Image.open('view.jpg')
+    flowers_image = Image.open('flowers.jpg')
 
-    # Coordinates must match exactly:
-    area = (350, 350, 500, 500)
 
-    # Paste image on top of other:
-    my_image.paste(ducky, area)
+    ##############################
 
-    # Show the result image:
-    my_image.show()
 
-    # Close both images:
-    my_image.close()
-    ducky.close()
+    # Display image mode:
+    # print(view_image.mode)
+
+
+    ##############################
+
+
+    # # Split into seperate channels:
+    # r, g, b = view_image.split()
+    #
+    # # Display seperate channels:
+    # r.show()
+    # g.show()
+    # b.show()
+
+
+    ##############################
+
+
+    # # Merge channels:
+    # r, g, b = view_image.split()
+    # merged_image = Image.merge("RGB", (r, g, b))
+    #
+    # # Merge in different order for effect:
+    # merged_image = Image.merge("RGB", (r, b, g))
+    # merged_image.show()
+
+
+    ###############################
+
+
+    # Merging two images:
+    # Separate Channels:
+    r1, g1, b1 = view_image.split()
+    r2, g2, b2 = flowers_image.split()
+
+    # Merge channels:
+    merged_image = Image.merge("RGB", (r2, g1, b2))
+
+    # Show the image:
+    merged_image.show()
+
+    # Save the image:
+    merged_image.save('cool_image.jpg')
+
+
+
+    # Close the images:
+    view_image.close()
+    flowers_image.close()
+
 
 
 # Call main:
