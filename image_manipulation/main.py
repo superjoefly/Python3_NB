b5d1a208a0333b214947395ea9527b22ccc557d1
superjoefly <superjoefly@gmail.com>
2018-06-02 14:17:38 -0600

    Completed image manipulation.

--- image_manipulation/main.py
+++ image_manipulation/main.py
@@ -1,5 +1,6 @@
 # Import Modules:
 from PIL import Image
+from PIL import ImageFilter
 
 
 # Define main:
@@ -23,6 +24,16 @@ def main():
 
     ###############################
 
+
+
+
+
+
+
+
+
+
+
     # # Image Croping:
     #
     # # Define the area (x, y TO x , y):
@@ -41,6 +52,16 @@ def main():
     # my_image.close()
 
     ################################
+
+
+
+
+
+
+
+
+
+
 
     # # Image Overlay:
     #
@@ -63,67 +84,138 @@ def main():
 
     ################################
 
-    # Working with Channels:
-
-    # Open the images:
-    view_image = Image.open('view.jpg')
-    flowers_image = Image.open('flowers.jpg')
 
 
-    ##############################
 
 
-    # Display image mode:
-    # print(view_image.mode)
 
 
-    ##############################
 
 
-    # # Split into seperate channels:
-    # r, g, b = view_image.split()
+
+    # # Working with Channels:
     #
-    # # Display seperate channels:
-    # r.show()
-    # g.show()
-    # b.show()
-
-
-    ##############################
-
-
+    # # Open the images:
+    # view_image = Image.open('view.jpg')
+    # flowers_image = Image.open('flowers.jpg')
+    #
+    #
+    # ##############################
+    #
+    #
+    # # Display image mode:
+    # # print(view_image.mode)
+    #
+    #
+    # ##############################
+    #
+    #
+    # # # Split into seperate channels:
+    # # r, g, b = view_image.split()
+    # #
+    # # # Display seperate channels:
+    # # r.show()
+    # # g.show()
+    # # b.show()
+    #
+    #
+    # ##############################
+    #
+    #
+    # # # Merge channels:
+    # # r, g, b = view_image.split()
+    # # merged_image = Image.merge("RGB", (r, g, b))
+    # #
+    # # # Merge in different order for effect:
+    # # merged_image = Image.merge("RGB", (r, b, g))
+    # # merged_image.show()
+    #
+    #
+    # ###############################
+    #
+    #
+    # # Merging two images:
+    # # Separate Channels:
+    # r1, g1, b1 = view_image.split()
+    # r2, g2, b2 = flowers_image.split()
+    #
     # # Merge channels:
-    # r, g, b = view_image.split()
-    # merged_image = Image.merge("RGB", (r, g, b))
+    # merged_image = Image.merge("RGB", (r2, g1, b2))
     #
-    # # Merge in different order for effect:
-    # merged_image = Image.merge("RGB", (r, b, g))
+    # # Show the image:
     # merged_image.show()
+    #
+    # # Save the image:
+    # merged_image.save('cool_image.jpg')
+    #
+    #
+    #
+    # # Close the images:
+    # view_image.close()
+    # flowers_image.close()
 
-
-    ###############################
-
-
-    # Merging two images:
-    # Separate Channels:
-    r1, g1, b1 = view_image.split()
-    r2, g2, b2 = flowers_image.split()
-
-    # Merge channels:
-    merged_image = Image.merge("RGB", (r2, g1, b2))
-
-    # Show the image:
-    merged_image.show()
-
-    # Save the image:
-    merged_image.save('cool_image.jpg')
+    ############################
 
 
 
-    # Close the images:
-    view_image.close()
-    flowers_image.close()
 
+
+
+    # # Transforms:
+    #
+    # my_image = Image.open('me.jpg')
+    #
+    # # # Resize:
+    # # new_image = my_image.resize((500, 250))
+    # # new_image.show()
+    #
+    # # # Flip:
+    # # flipped_image = my_image.transpose(Image.FLIP_TOP_BOTTOM)
+    # # flipped_image.show()
+    #
+    # # Spin:
+    # spun_image = my_image.transpose(Image.ROTATE_90)
+    # spun_image.show()
+    #
+    # # Close the image:
+    # my_image.close()
+
+    ############################
+
+
+
+
+
+
+
+
+    # # Mode Conversion and ImageFilters:
+    #
+    # # Open the image:
+    # my_image = Image.open('me.jpg')
+    #
+    # # # Convert to black and white:
+    # # black_white = my_image.convert('L')
+    # # black_white.show()
+    #
+    #
+    # # # Blur image (ImageFilter):
+    # # blurred = my_image.filter(ImageFilter.BLUR)
+    # # blurred.show()
+    #
+    #
+    # # # Detail image (ImageFilter):
+    # # detailed = my_image.filter(ImageFilter.DETAIL)
+    # # detailed.show()
+    #
+    #
+    # # # Edges image (ImageFilter):
+    # # edges = my_image.filter(ImageFilter.FIND_EDGES)
+    # # edges.show()
+    #
+    #
+    # # Close the image:
+    # my_image.close()
 
 
 # Call main:
