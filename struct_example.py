53ef3397fd0b0a76fe3b01102f0ee8c70e98265b
superjoefly <superjoefly@gmail.com>
2018-06-02 14:56:52 -0600

    Completed structs example.

---
+++ struct_example.py
@@ -0,0 +1,28 @@
+"""Struct - Packing and Unpacking Bytes Data"""
+
+from struct import *
+
+
+# Define main:
+def main():
+    """The main function"""
+    # Store as bytes data (int, int, float):
+    packed_data = pack('iif', 5, 4, 3.33)
+
+    # Display bytes data:
+    print(packed_data)
+
+    # Display bytes size:
+    print(calcsize('i'))
+    print(calcsize('if'))
+    print(calcsize('iif'))
+
+    # Unpack to human-readable data:
+    unpacked_data = unpack('iif', packed_data)
+    print(unpacked_data)
+    print('----------------------------')
+    print(unpack('iif', b'\x05\x00\x00\x00\x04\x00\x00\x00\xb8\x1eU@'))
+
+
+# Call main:
+main()
