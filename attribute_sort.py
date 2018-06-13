04f52b1fcc3b2e24bf8b1278f6da5297ce47c8c5
superjoefly <superjoefly@gmail.com>
2018-06-13 14:09:38 -0600

    Completed attribute sort example.

---
+++ attribute_sort.py
@@ -0,0 +1,50 @@
+"""Sort Objects by Attribute"""
+
+
+# Import Modules:
+from operator import attrgetter
+
+# Define main:
+def main():
+    """The main function"""
+
+    # Create the User class:
+    class User:
+        """User Class"""
+
+        # Initialize objects with name and user_id:
+        def __init__(self, name, user_id):
+            self.name = name
+            self.user_id = user_id
+
+        # String representation of object:
+        def __repr__(self):
+            return self.name + ':' + str(self.user_id)
+
+    # List of user objects:
+    users = [
+        User('John', 43),
+        User('Craig', 5),
+        User('Billy', 61),
+        User('Gina', 2),
+        User('Jessica', 8)
+    ]
+
+    # Display each user:
+    for user in users:
+        print(user)
+
+    print("-----------------")
+
+    # Display list of users sorted by name:
+    for user in sorted(users, key=attrgetter('name')):
+        print(user)
+
+    print("-----------------")
+
+    # Display list of users sorted by user_id:
+    for user in sorted(users, key=attrgetter('user_id')):
+        print(user)
+
+# Call main:
+main()
