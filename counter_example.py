d3c15ad46c6019d3ba610b55ccc66f79fa395a38
superjoefly <superjoefly@gmail.com>
2018-06-13 18:16:52 -0600

    Modularized program.

--- counter_example.py
+++ counter_example.py
@@ -3,24 +3,46 @@
 # Import Modules:
 from collections import Counter
 
+# Define main:
 def main():
-    """Word Frequency Counter"""
+    """The main function"""
 
-    # Assign sentence
-    sentence = "The momentous change licenses the rain. The current " \
-    "debugs the record. The son reinforces the start. The invincible " \
-    "mist relates the fear. The maniacal insurance resolves the " \
-    "son."
+    print()
 
-    # Split into list of words
+    # Get sentence for word frequency count:
+    sentence = input("Enter a sentence for the word frequency count: ")
+
+    print()
+
+    # Define level of frequency:
+    level = int(input("Enter a non-negative integer for the level of " \
+    "frequency. For example, enter '3' to get the top 3 words: "))
+
+    print()
+
+    # Get frequent words:
+    frequent_words = word_frequency(sentence, level)
+
+    # Display frequent words:
+    print(frequent_words)
+
+
+# Define word_count:
+def word_frequency(sentence, level):
+    """Count word frequency"""
+
+    # Split into list of words:
     words = sentence.split()
 
+    # Create Counter object:
     counter = Counter(words)
 
-    top_three = counter.most_common(3)
+    # Get top three most common words:
+    result = counter.most_common(level)
 
-    print(top_three)
+    # Display top three most common words:
+    return result
 
 
-
+# Call main:
 main()
